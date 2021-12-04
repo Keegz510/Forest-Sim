c57e9a8b95e17819d00afb3b755ae1f84a1407e0
Keegz510 <s200322@students.aie.edu.au>
2021-12-04 11:06:32 +1100

    Commented Code

--- ForestSim/Source/Private/Engine/Game.cpp
+++ ForestSim/Source/Private/Engine/Game.cpp
@@ -1,27 +1,38 @@
 #include "../../Public/Engine/Game.h"
+#include "../../Public/Engine/Globals.h"
 #include <raylib.h>
 
 Game::Game(const WindowSettings windowSettings) : window(windowSettings)
 {
-	
+	Globals::Setup(this);			// Add this class to the globals
 }
 
 Game::~Game()
 {
 }
 
+/// <summary>
+/// Handles Creating/Setting up the game/game window
+/// </summary>
 void Game::Initialize()
 {
 
 	// Create the new window
 	InitWindow(window.ScreenWidth, window.ScreenHeight, window.WindowTitle);
 	SetTargetFPS(60);
+
+	
 }
 
+
+/// <summary>
+/// Handles updating all objects
+/// </summary>
 void Game::BaseUpdate()
 {
 	while (!WindowShouldClose())
 	{
+		deltaTime = GetFrameTime();					// Get the detlaTime
 		// Update base details here!
 
 		BeginDrawing();
